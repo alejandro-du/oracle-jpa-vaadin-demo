776904a1b232dacb48591206281eaacee95aab47
alejandro-du <alejandro.d.a@gmail.com>
2017-02-01 13:18:54 +0200

    Upgraded to Vaadin 8.0.0.beta1

--- src/main/java/com/example/VaadinUI.java
+++ src/main/java/com/example/VaadinUI.java
@@ -2,7 +2,6 @@ package com.example;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.TextField;
@@ -19,23 +18,23 @@ public class VaadinUI extends UI {
     @Inject
     private EmployeeRepository repository;
 
-    private Grid grid = new Grid();
+    private Grid<Employee> grid = new Grid<>();
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         TextField filter = new TextField("Filter by name:");
-        filter.addTextChangeListener(e -> updateGrid(e.getText()));
+        filter.addValueChangeListener(e -> updateGrid(e.getValue()));
+
+        grid.addColumn(Employee::getName).setCaption("Name");
+        grid.addColumn(Employee::getEmail).setCaption("Email");
 
         VerticalLayout layout = new VerticalLayout(filter, grid);
-        layout.setMargin(true);
-        layout.setSpacing(true);
         setContent(layout);
         updateGrid("");
-        grid.setColumns("name", "email");
     }
 
     private void updateGrid(String filter) {
         List<Employee> employees = repository.findByNameLikeIgnoreCase("%" + filter + "%");
-        grid.setContainerDataSource(new BeanItemContainer<>(Employee.class, employees));
+        grid.setItems(employees);
     }
 }
